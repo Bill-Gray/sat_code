e03c80704c33cd2374912c86a88532157c5c7a19
Bill-Gray <pluto@projectpluto.com>
2025-05-24 09:40:04 -0400

    'sat_eph' would not emit a header if the start of the ephems you generated lacked TLE data.  Also,  fixed a C90 compliance issue.

--- sat_eph.c
+++ sat_eph.c
@@ -164,7 +164,7 @@ static int show_ephems_from( const char *path_to_tles, const ephem_t *e,
 {
    FILE *ifile;
    char line0[100], line1[100], line2[100];
-   int show_it = 1;
+   int show_it = 1, header_shown = 0;
    double jd_tle = 0., tle_range = 1e+10, abs_mag = 0.;
    const bool is_geocentric = (e->rho_sin_phi == 0. && e->rho_cos_phi == 0.);
    static const char *header_text =
@@ -232,10 +232,11 @@ static int show_ephems_from( const char *path_to_tles, const ephem_t *e,
                double ra_motion, dec_motion;
                const char *format_string;
 
-               if( !i)
+               if( !header_shown)
                   {
                   char *tptr;
 
+                  header_shown = 1;
                   printf( "\nEphemerides for %05d = %s%.2s-%s\n",
                               tle.norad_number,
                               (atoi( tle.intl_desig) > 57000) ? "19" : "20",
@@ -383,9 +384,11 @@ int generate_artsat_ephems( const char *path_to_tles, const ephem_t *e)
          }
       if( !memcmp( buff, "# ID:", 5))
          {
+         int i;
+
          if( buff[5] != ' ' || buff[11] != ' ' || buff[12] != ' ')
             fprintf( stderr, "BAD LINE %s\n", buff);
-         for( int i = 6; i < 10; i++)
+         for( i = 6; i < 10; i++)
             if( !isdigit( buff[i]) || !isdigit( buff[i + 7]))
                {
                printf( "BAD LINE (2) %s\n", buff);
